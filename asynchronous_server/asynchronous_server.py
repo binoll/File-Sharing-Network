1750303d6bf1fb9025875676efc89f77ebdef9ac
binoll <onionlsfa@gmail.com>
2024-05-02 08:18:35 +0300

    refactor: refactored code

--- asynchronous_server/asynchronous_server.py
+++ asynchronous_server/asynchronous_server.py
@@ -1,10 +1,10 @@
 import asyncio
 import socket
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Optional, Any, Coroutine
 
 BUFFER_SIZE = 1024
 BACKLOG = 100
-commands = ["list", "get", "exit", "error", "exist"]
+commands = ['list', 'get', 'exit', 'error', 'exist']
 
 
 class FileInfo:
@@ -37,10 +37,8 @@ class Connection:
         print(f'[*] Server is listening on port for communicate: {socket_communicate.getsockname()[1]}.')
         print(f'[*] Server is listening on port for listening: {socket_listen.getsockname()[1]}.')
 
-        await asyncio.gather(
-            self.accept_connections(socket_listen, socket_communicate),
-            self.handle_clients()
-        )
+        await self.accept_connections(socket_listen, socket_communicate)
+        await self.handle_clients()
 
     async def accept_connections(self, socket_listen, socket_communicate):
         while True:
@@ -55,7 +53,7 @@ class Connection:
                   f' {client_addr_communicate[0]}:{client_addr_communicate[1]}.')
 
             async with self.synchronization_lock:
-                synchronization = await self.synchronization(client_socket_listen, client_socket_communicate)
+                synchronization = self.synchronization(client_socket_listen, client_socket_communicate)
 
                 if synchronization:
                     self.sockets.append((client_socket_listen, client_socket_communicate))
@@ -67,7 +65,7 @@ class Connection:
     async def handle_clients(self) -> None:
         while True:
             for client_socket_listen, client_socket_communicate in self.sockets:
-                command = self.receive_message(client_socket_listen)
+                command = self.receive_message(client_socket_listen, BUFFER_SIZE)
 
                 if not command:
                     continue
@@ -75,7 +73,7 @@ class Connection:
                 if command == commands[0]:
                     await self.send_list(client_socket_listen)
                 elif command.startswith(commands[1]):
-                    filename = command.split(":")[1]
+                    filename = command.split(':')[1]
                     await self.send_file(client_socket_listen, filename)
                 elif command == commands[2]:
                     self.remove_clients((client_socket_listen, client_socket_communicate))
@@ -83,35 +81,39 @@ class Connection:
                     client_socket_communicate.close()
                     print('[+] Success: Client disconnected.')
 
-    async def synchronization(self, client_socket_listen: socket.socket,
-                              client_socket_communicate: socket.socket) -> bool:
+    def synchronization(self, client_socket_listen: socket.socket,
+                        client_socket_communicate: socket.socket) -> bool:
         command_error = commands[3]
 
-        message = self.receive_message(client_socket_listen)
+        message = self.receive_message(client_socket_listen, BUFFER_SIZE)
         if not message or message == command_error:
             return False
 
-        message_size = self.process_response(message)
+        message_size, message = self.process_response(message)
         if message_size is None:
             return False
 
         while message_size > 0:
             try:
                 for file_info in message.split(' '):
+                    if not file_info:
+                        break
+
                     filename, file_size_str, file_hash = file_info.split(':')
                     file_size = int(file_size_str)
-                    self.store_files((client_socket_listen, client_socket_communicate), filename,
-                                     file_size, file_hash)
+                    self.store_files((client_socket_listen, client_socket_communicate), filename, file_size, file_hash)
             except ValueError as err:
-                print("[-] Error:", err)
+                print('[-] Error:', err)
                 return False
             except Exception as err:
-                print("[-] Error:", err)
+                print('[-] Error:', err)
                 return False
 
             message_size -= len(message)
+            if message_size <= 0:
+                break
 
-            message = self.receive_message(client_socket_listen)
+            message = self.receive_message(client_socket_listen, message_size)
             if not message or message == command_error:
                 return False
 
@@ -119,63 +121,64 @@ class Connection:
         return True
 
     @staticmethod
-    def send_message(self, socket: socket.socket, message: str) -> int:
+    def send_message(sock: socket.socket, message: str) -> Coroutine[Any, Any, None]:
         buffer = bytearray(message.encode())
-        return self.send_bytes(socket, buffer)
+        return Connection.send_bytes(sock, buffer)
 
     @staticmethod
-    def receive_message(socket: socket.socket, size: int) -> str | None:
-        buffer = bytearray(size)
-        bytes_received = socket.recv_into(buffer, size)
-
-        if bytes_received <= 0:
+    def receive_message(sock: socket.socket, size: int) -> Optional[str]:
+        buffer = Connection.receive_bytes(sock, size)
+        if not buffer:
             return None
-        else:
-            return buffer.decode()
-
-    @staticmethod
-    def send_bytes(socket: socket.socket, buffer: bytearray) -> int:
-        return socket.send(buffer)
-
-    @staticmethod
-    def receive_bytes(socket: socket.socket, buffer: bytearray, size: int) -> int:
-        return socket.recv_into(buffer, size)
-
-    @staticmethod
-    def process_response(message: str) -> int | None:
         try:
-            pos = message.find(':')
-            if pos == -1:
-                return None
+            str(buffer)
+        except ValueError:
+            return None
+        return str(buffer)
 
-            size = int(message[:pos])
-            return size
+    @staticmethod
+    def send_bytes(sock: socket.socket, buffer: bytes) -> Coroutine[Any, Any, None]:
+        return asyncio.get_event_loop().sock_sendall(sock, buffer)
+
+    @staticmethod
+    def receive_bytes(sock: socket.socket, size: int) -> Coroutine[Any, Any, bytes] | None:
+        buffer = asyncio.get_event_loop().sock_recv(sock, size)
+        if not buffer:
+            return None
+        return buffer
+
+    @staticmethod
+    def process_response(message: str) -> tuple[int, str] | None:
+        try:
+            size, _, rest = message.partition(':')
+            return int(size), rest
         except ValueError:
             return None
 
-    async def send_list(self, socket: socket.socket) -> int:
+    async def send_list(self, sock: socket.socket) -> int | None:
         files = self.get_list_files()
 
         try:
             list_str = ' '.join(files)
             message_size = len(list_str)
         except Exception as e:
-            print("[-] Error:", e)
+            print('[-] Error:', e)
             return -1
 
         message = f'{message_size}: {list_str}'
-        bytes_sent = self.send_message(socket, message)
-        return bytes_sent + len(str(message_size) + ':')
+        bytes_sent = await self.send_message(sock, message)
+        return bytes_sent
 
-    async def send_file(self, socket: socket.socket, filename: str) -> int:
+    async def send_file(self, sock: socket.socket, filename: str) -> int | None:
         command_error = commands[3]
         command_exist = commands[4]
         message_size = self.get_size(filename)
-        real_filename = filename if not self.is_filename_modify(filename) else self.remove_index(filename)
         total_bytes = 0
 
-        if self.is_filename_change(socket, filename):
-            self.send_message(socket, command_exist)
+        real_filename = filename if not self.is_filename_modify(filename) else self.remove_index(filename)
+
+        if self.is_filename_change(sock, filename):
+            await self.send_message(sock, command_exist)
             return 0
 
         try:
@@ -184,46 +187,49 @@ class Connection:
             print('[-] Error:', e)
             return -1
 
-        bytes_sent = self.send_message(socket, message)
-        if bytes_sent < 0:
+        bytes_sent = await self.send_message(sock, message)
+        if bytes_sent is None:
             return -1
 
         sockets = self.find_socket(filename)
-        sockets = [(pair[0], pair[1]) for pair in sockets if pair[0] != socket and pair[1] != socket]
+        sockets = [(pair[0], pair[1]) for pair in sockets if pair[0] != sock and pair[1] != sock]
 
         if not sockets:
             return -1
 
         for i, (_, client_socket_communicate) in enumerate(sockets):
             for offset in range(0, message_size, BUFFER_SIZE):
-                buffer = bytearray()
-                chunk_size = min(message_size - offset, BUFFER_SIZE)
                 client_socket_communicate = sockets[i % len(sockets)][1]
+                chunk_size = min(message_size - offset, BUFFER_SIZE)
 
                 message = f'{commands[1]}:{offset}:{chunk_size}:{real_filename}'
                 if not self.check_connection(client_socket_communicate):
                     return -1
 
                 bytes_sent = self.send_message(client_socket_communicate, message)
-                if bytes_sent < 0:
+                if bytes_sent is None:
                     continue
 
                 try:
-                    bytes_received = self.receive_bytes(client_socket_communicate, buffer)
-                except socket.timeout:
+                    buffer = self.receive_bytes(client_socket_communicate, chunk_size)
+                except sock.timeout:
                     continue
                 except Exception as e:
                     print('[-] Error:', e)
                     continue
 
-                if not bytes_received:
+                if not buffer:
                     continue
 
-                if bytes_received == command_error:
-                    return -2
+                try:
+                    buffer_str = str(buffer)
+                    if buffer_str == command_error:
+                        return -2
+                except ValueError:
+                    pass
 
-                bytes_sent = self.send_bytes(socket, buffer)
-                if bytes_sent < 0:
+                bytes_sent = await self.send_bytes(sock, buffer)
+                if bytes_sent is None:
                     continue
 
                 total_bytes += bytes_sent
@@ -307,9 +313,9 @@ class Connection:
                 return value.is_filename_modify
         return False
 
-    def is_filename_change(self, socket: socket.socket, filename: str) -> bool:
+    def is_filename_change(self, sock: socket.socket, filename: str) -> bool:
         for key, value in self.storage.items():
-            if value.filename == filename and (key[0] == socket or key[1] == socket):
+            if value.filename == filename and (key[0] == sock or key[1] == sock):
                 return value.is_filename_changed
         return False
 
