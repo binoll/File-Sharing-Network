5c58adc6707ef113b443fcb590668a04fff2772d
binoll <onionlsfa@gmail.com>
2024-06-01 09:45:23 +0300

    fix: fixed bugs

--- switch/config.c
+++ switch/config.c
@@ -2,12 +2,13 @@
 
 int read_config(const char* filename, struct config* cfg) {
 	FILE* file = fopen(filename, "r");
+	char line[BUFFER_SIZE];
+
 	if (file == NULL) {
 		fprintf(stderr, "Could not open config file %s\n", filename);
 		return EXIT_FAILURE;
 	}
 
-	char line[BUFFER_SIZE];
 	while (fgets(line, sizeof(line), file) != NULL) {
 		if (sscanf(line, "modify_flag=%d", &cfg->modify_flag) == 1) {
 			continue;
